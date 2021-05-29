766929b597aca9517904b9d7d18fc0fa28aaeff8
Eduardo <eduardo0391@gmail.com>
2021-05-29 13:44:14 -0300

    Register page

--- src/login/Register.js
+++ src/login/Register.js
@@ -3,10 +3,55 @@ import axios from 'axios';
 
 function Register(props)
 {
-   return (
-    <div>
+    const [user, setUser] = useState({User:"", Password:"", RepeatPassword:"", Email:"", Name:"" })
+    const Register = (e) => {
 
-    </div>
+    }
+     const onChange = (e) => {
+        setUser(e.target.value);
+     }
+    return (
+       <div className="auth-wrapper">  
+            <div className="auth-inner">
+                <form onSubmit={Register}>
+                        <h3>Sign In</h3>
+
+                        <div className="form-group">
+                            
+                            <label>User</label>
+                            <input type="Text" class="form-control" value={user.User} 
+                            placeholder="User" name="User" onChange={e=> setUser(e.target.value)}>
+                            </input>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input type="Password" class="form-control" value={user.Password}
+                            placeholder="Password" name="Password" onChange={onChange} ></input>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Repeat Password</label>
+                            <input type="Password" class="form-control" value={user.RepeatPassword}
+                            placeholder="Repeat Password" name="RepeatPassword" onChange={onChange}></input>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input type="Name" class="form-control" value={user.Name}
+                            placeholder="Name"name="Name" onChange={onChange}></input>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Email</label>
+                            <input type="Email" class="form-control" value={user.Email}
+                            placeholder="Email" name="Email" onChange={onChange}></input>
+                        </div>
+
+                        <button type="button" className="btn btn-primary btn-block">Sign up</button>
+                </form>
+            </div>
+        </div>
 ) 
 }
 export default Register;
